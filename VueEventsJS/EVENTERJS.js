0280d923a5a80c2a82a8db53587e36782e8407da
Delarule <151906991+Delarule@users.noreply.github.com>
2023-12-20 13:07:03 +0300

    Add files via upload

---
+++ VueEventsJS/EVENTERJS.js
@@ -0,0 +1,14 @@
+var loop;
+ function func() {
+    var result;
+     var Sybwol = Number(document.getElementById("Sybwol").value(0));
+   switch (op) {
+      case '+':
+        result = Symbol + 2;
+        break;
+      case '-':
+        result = Symbol - 2;
+        break;
+    }
+
+  }
